26126fe9d5d25ace4b856695eaff6e6e0bb90e89
Ameya Naik <ameya.n2007@gmail.com>
2024-01-20 14:18:20 -0800

    Commit from TeleportHQ

--- src/components/event-name-and-time-simple.js
+++ src/components/event-name-and-time-simple.js
@@ -6,42 +6,29 @@ import './event-name-and-time-simple.css'
 
 const EventNameAndTimeSimple = (props) => {
   return (
-    <animate-on-reveal
-      animation="fadeIn"
-      duration="1s"
-      delay="500ms"
-      direction="normal"
-      easing="ease-in"
-      iteration="1"
-      className={`event-name-and-time-simple-animate-on-reveal featuresCard ${props.rootClassName} `}
+    <div
+      className={`event-name-and-time-simple-feature-card featuresCard ${props.rootClassName} `}
     >
-      <div
-        data-thq-animate-on-reveal="true"
-        className={`event-name-and-time-simple-feature-card featuresCard ${props.rootClassName} `}
-      >
-        <div className="event-name-and-time-simple-container">
-          <animate-on-reveal
-            animation="none"
-            duration="300ms"
-            delay="0s"
-            direction="normal"
-            easing="ease"
-            iteration="1"
-            className="event-name-and-time-simple-animate-on-reveal1"
+      <div className="event-name-and-time-simple-container">
+        <animate-on-reveal
+          animation="none"
+          duration="300ms"
+          delay="0s"
+          direction="normal"
+          easing="ease"
+          iteration="1"
+          className="event-name-and-time-simple-animate-on-reveal"
+        >
+          <span
+            data-thq-animate-on-reveal="true"
+            className="event-name-and-time-simple-text"
           >
-            <span
-              data-thq-animate-on-reveal="true"
-              className="event-name-and-time-simple-text"
-            >
-              {props.Time}
-            </span>
-          </animate-on-reveal>
-          <span className="event-name-and-time-simple-text1">
-            {props.Event}
+            {props.Time}
           </span>
-        </div>
+        </animate-on-reveal>
+        <span className="event-name-and-time-simple-text1">{props.Event}</span>
       </div>
-    </animate-on-reveal>
+    </div>
   )
 }
 
