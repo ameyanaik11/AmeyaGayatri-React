91cb89811116da7408db89934b0db0940e3783e2
Ameya Naik <ameya.n2007@gmail.com>
2024-01-24 22:59:59 -0800

    Commit from TeleportHQ

--- src/index.js
+++ src/index.js
@@ -8,18 +8,18 @@ import {
 } from 'react-router-dom'
 
 import './style.css'
+import Home20230123 from './views/home-20230123'
 import Temp from './views/temp'
 import Home from './views/home'
-import Home20230123 from './views/home-20230123'
 import NotFound from './views/not-found'
 
 const App = () => {
   return (
     <Router>
       <Switch>
+        <Route component={Home20230123} exact path="/home-20230123" />
         <Route component={Temp} exact path="/temp" />
         <Route component={Home} exact path="/" />
-        <Route component={Home20230123} exact path="/home-20230123" />
         <Route component={NotFound} path="**" />
         <Redirect to="**" />
       </Switch>
